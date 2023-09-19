421b861ba1add0f0adcb8c5ddaef4e48d550db98
Arthur <arthurhenrique0708@gmail.com>
2023-09-19 09:51:43 -0300

    pagina de admin feita

--- site/src/pages/adm/adm-pedidos-concluidos/index.js
+++ site/src/pages/adm/adm-pedidos-concluidos/index.js
@@ -19,12 +19,12 @@ export default function AdmPedidosConcluidos() {
                 <button><img src="/assets/images/adm/1617460.svg" alt="" /></button>
             </article>
 
-            <article>
-                <div><h1>PedidosConcluídos</h1></div>
+            <article id='tabela'>
+                <h1>Pedidos Concluídos</h1>
                 <table>
-                    <thead>
+                    <thead className='tablehead'>
                         <tr>
-                            <th>Id <button><img src="" alt="" /></button></th>
+                            <th>Id</th>
                             <th>Nome</th>
                             <th>Valor Uni.</th>
                             <th>Total</th>
@@ -33,6 +33,26 @@ export default function AdmPedidosConcluidos() {
                             <th>Status</th>
                         </tr>
                     </thead>
+                    <tbody className='tablebody'>
+                        <tr>
+                            <td>1</td>
+                            <td>Brincos de Cristal</td>
+                            <td>54</td>
+                            <td>54</td>
+                            <td>1</td>
+                            <td>12390/*123/123</td>
+                            <td>Vendido</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Brincos de Cristal</td>
+                            <td>54</td>
+                            <td>54</td>
+                            <td>1</td>
+                            <td>12390/*123/123</td>
+                            <td>Vendido</td>
+                        </tr>
+                    </tbody>
                 </table>
             </article>
         </section>
