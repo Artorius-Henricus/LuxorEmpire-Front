3f791302a09f5a6a7c0d51955b011ce68fef98a6
Arthur <arthurhenrique0708@gmail.com>
2023-09-19 08:37:46 -0300

    Adicionando imagens

--- site/src/pages/adm/adm-pedidos-concluidos/index.js
+++ site/src/pages/adm/adm-pedidos-concluidos/index.js
@@ -18,6 +18,23 @@ export default function AdmPedidosConcluidos() {
                 <input type='text' />
                 <button><img src="/assets/images/adm/1617460.svg" alt="" /></button>
             </article>
+
+            <article>
+                <div><h1>PedidosConcluídos</h1></div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id <button><img src="" alt="" /></button></th>
+                            <th>Nome</th>
+                            <th>Valor Uni.</th>
+                            <th>Total</th>
+                            <th>Quant</th>
+                            <th>Data Compra</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                </table>
+            </article>
         </section>
     </div>
     )
