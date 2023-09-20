66189859c8f67ea7c6fe64a4d934457a8fecc5ba
Arthur <arthurhenrique0708@gmail.com>
2023-09-20 15:23:32 -0300

    Telas do Adm concluídas com excessão da 1° Tela

--- site/src/components/adm/menubar/index.js
+++ site/src/components/adm/menubar/index.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './index.scss';
 
 import {Link} from 'react-router-dom'
 
 export default function CompMenuBar() {
+    const [shoff, setShoff] = useState(false);
+    const [shoff2, setShoff2] = useState(false);
 
     return (
         <div className="comp-menu-bar">
@@ -16,20 +19,31 @@ export default function CompMenuBar() {
             <img src="/assets/images/rodape/LogoResu.svg" alt="" id='logoadm'/>
 
 
-            <Link to='' className='btmn'>
+            <Link to='/adm/central' className='btmn'>
                 <img src="/assets/images/menubar/house-vector-icon.svg" alt="" />
                 Central
             </Link>
 
-            <Link to='' className='btmn'>
-                <img src="/assets/images/menubar/2652218.svg" alt="" />
-                Produtos
-            </Link>
+            <button className='btsec' onMouseEnter={() => {setShoff(true)}} onMouseLeave={() => {setShoff(false)}}>
+                <span>
+                    <img src="/assets/images/menubar/2652218.svg" alt="" />
+                    Pedidos
+                </span>
+                <div className={`off ${shoff ? 'show' : ''}`}>
+                    <Link to='/adm/pedidos-concluidos' className='btextra'>Pedidos Concluídos</Link>
+                    <Link to='/adm/pedidos-andamento' className='btextra'>Pedidos em Andamento</Link>
+                </div>
+            </button>
 
-            <Link to='' className='btmn'>
-                <img src="/assets/images/menubar/126391.svg" alt="" />
-                Ferramentas
-            </Link>
+            <button className='btsec' onMouseEnter={() => {setShoff2(true)}} onMouseLeave={() => {setShoff2(false)}}>
+                <span>
+                    <img src="/assets/images/menubar/126391.svg" alt="" />
+                    Ferramentas
+                </span>
+                <div className={`off ${shoff2 ? 'show' : ''}`}>
+                    <Link to='/adm/cadastrar-produto' className='btextra'>Cadastrar Produto</Link>
+                </div>
+            </button>
         </div>
     )
 }
--- site/src/index.js
+++ site/src/index.js
@@ -12,6 +12,7 @@ import AdmLogin from './pages/adm/adm-login';
 import Cartoes from './pages/site/cartoespag/cartoes';
 import AdmPedidosConcluidos from './pages/adm/adm-pedidos-concluidos';
 import AdmPedidosAndamento from './pages/adm/adm-pedidos-andamentoi';
+import AdmCadastroProduto from './pages/adm/adm-cadastro-produto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,6 +23,7 @@ root.render(
         <Route path='/adm/central' element={<AdmCentral />} />
         <Route path='/adm/pedidos-concluidos' element={<AdmPedidosConcluidos />} />
         <Route path='/adm/pedidos-andamento' element={<AdmPedidosAndamento />} />
+        <Route path='/adm/cadastrar-produto' element={<AdmCadastroProduto />} />
         <Route path='/adm' element={<AdmLogin />} />
         <Route path='/cartoes' element={<Cartoes/>} />
       </Routes>
