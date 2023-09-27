5a48c65b332cb186b9df492064d50073eed6dd26
ArthurC <arthur.zika2077@gmail.com>
2023-09-27 09:36:45 -0300

    ainda finalizando a pagina de produtos

--- site/src/pages/site/tela-produto/index.js
+++ site/src/pages/site/tela-produto/index.js
@@ -37,7 +37,7 @@ export default function Paginaproduto() {
                         <img src='/assets/images/produtos/Cartão Icon.svg' />
                         <h2>10x de R$valor sem juros</h2>
                     </div>
-                    <div className='dir-linha2'>
+                    <div className='dir-linha-button'>
                         <div className='dir-linha-main'>
                             <button>-</button>
                             <p>1</p>
@@ -46,6 +46,22 @@ export default function Paginaproduto() {
                             
                         <button id='but-dir'>Adicionar ao Carrinho</button>
                     </div>
+
+                    <div className='dir-linha-img'>
+                        <img src='/assets/images/produtos/Caixa 1.svg'/>
+                        <p>Prazo de produção:</p>
+                        <p id='txt-light'>2 dias úteis</p>
+                    </div>
+
+                    {/* <div className='dir-linha-segurança'>
+                        <img src='/assets/images/produtos/check-mark-icon-vector-removebg-preview 1.svg' />
+                        <div className='txt-column'>
+                            <p>Certificado de Garantia e Autenticidade</p>
+                            <p>Todas as Joias acompanham certificado de garantia e autenticidade do Ouro 18k, Diamantes e Pedras Preciosas.</p>
+
+                        </div>
+                    </div> */}
+
                 </div>
             </section>
         </div>
