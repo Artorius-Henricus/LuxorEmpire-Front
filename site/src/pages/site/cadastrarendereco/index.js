c2dd3985460183e6bc3034caea43ec8cf124946e
Artorius <arthurhenrique0708@gmail.co>
2023-09-27 08:00:06 -0300

    Tela de Cadastrar Endereço feita!

--- site/src/pages/site/cadastrarendereco/index.js
+++ site/src/pages/site/cadastrarendereco/index.js
@@ -9,7 +9,53 @@ export default function CadastrarEndereco() {
         <div className='pagina-cadastrar-endereco'>
             <CompCabecalho />
             <div className='corp'>
+                <article className='inputs'>
+                    <h1>Adicionar um novo endereço</h1>
 
+                    <div>
+                        <label>País/Região</label>
+                        <select>
+                            <option>Selecione um País</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Nome</label>
+                        <input type='text' />
+                    </div>
+
+                    <div>
+                        <label>Cep</label>
+                        <input type='text' />
+                    </div>
+
+                    <div>
+                        <label>Endereço</label>
+                        <input type='text' />
+                    </div>
+
+                    <div>
+                        <label>Número da Residência</label>
+                        <input type='text' />
+                    </div>
+
+                    <div>
+                        <label>Bairro</label>
+                        <input type='text' />
+                    </div>
+                    
+                    <div>
+                        <label>Cidade</label>
+                        <input type='text' placeholder={'Insira o CEP acima para preencher a cidade'} className='unaltera' readOnly/>
+                    </div>
+
+                    <div>
+                        <label>Estado</label>
+                        <input type='text' placeholder={'Insira o CEP acima para preencher o estado'} className='unaltera' readOnly/>
+                    </div>
+
+                    <button>Salvar Endereço</button>
+                </article>
             </div>
             <CompRodape />
         </div>
