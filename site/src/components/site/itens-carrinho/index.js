5c43ab3d84f5b4983ac81955b2b606140b5f3b23
Arthur Henrique <92705939+Artorius-Henricus@users.noreply.github.com>
2023-10-06 19:04:46 -0300

    teste

--- site/src/components/site/itens-carrinho/index.js
+++ site/src/components/site/itens-carrinho/index.js
@@ -16,9 +16,9 @@ export default function CompCarrinho() {
                 <p id='number'>R$ 000,00</p>
 
             <div className='quant'>
-                <div> + </div>
-                <div> 0 </div>
-                <div> - </div>
+                <button> + </button>
+                <h1> 0 </h1>
+                <button> - </button>
             </div>
 
             <p id='number'>R$ 000,00</p>
