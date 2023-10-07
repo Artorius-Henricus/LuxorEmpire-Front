1966fb7ea8299ae96cf4cce94a994e0ab8337a62
Arthur Henrique <92705939+Artorius-Henricus@users.noreply.github.com>
2023-10-06 22:53:00 -0300

    novo gráfico base

--- site/src/pages/adm/adm-central/index.js
+++ site/src/pages/adm/adm-central/index.js
@@ -1,47 +1,32 @@
 import './index.scss';
 import CompMenuBar from '../../../components/adm/menubar';
 
-import { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
+import { useEffect } from 'react';
+import { Chart } from "react-google-charts";
 
 export default function AdmCentral() {
-    const chartRef = useRef(null);
-    const chartInstance = useRef(null);
-  
-    useEffect(() => {
-      // Certifique-se de que o gráfico anterior seja destruído antes de criar um novo
-      if (chartInstance.current) {
-        chartInstance.current.destroy();
-      }
-      const ctx = chartRef.current.getContext('2d');
-      chartInstance.current = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: ['Homens', 'Mulheres', 'Outros'],
-          datasets: [
-            {
-              label: 'Novos Usuários',
-              data: [12, 19, 3],
-              backgroundColor: ['rgb(0, 140, 255)', 'rgb(228, 120, 255)', 'rgb(199, 199, 199)'],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-    }, []);
+    const data = [
+      ["Novos Usuários", "Quantidade"],
+      ["Homens", 11],
+      ["Mulheres", 10]
+    ];
+    
+    const options = {
+      title: "Novos Usuários",
+    };
 
     return(
         <div className="pagina-adm-central">
             <CompMenuBar />
 
-            <canvas ref={chartRef} width="400" height="200"></canvas>
+            
+            <Chart
+              chartType="PieChart"
+              data={data}
+              options={options}
+              width={"100%"}
+              height={"400px"}
+            />
         </div>
     )
 }
