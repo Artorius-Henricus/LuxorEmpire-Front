23b3d9c753f2bce2884359cc85fbfb551a3d8512
Arthur César <Arthurzika.2077@gmail.com>
2023-10-15 20:01:46 -0300

    começando a pagina de market, não consegui fazer muita coisa pq tive q resolver varias coisas aq em casa, foi mal

--- site/src/pages/site/market/index.js
+++ site/src/pages/site/market/index.js
@@ -1,10 +1,16 @@
 import './index.scss'
 import CompCabecalho from '../../../components/site/cabecalho'
-
+import CompIndicacoes from '../../../components/site/indicacoes-produto';
 export default function Paginamarket () {
     return(
         <div className='pagina-market'>
             <CompCabecalho></CompCabecalho>
+            
+            
+            <div className='section-mid'>
+                <CompIndicacoes/>
+            
+            </div>
         </div>
     )
 }
