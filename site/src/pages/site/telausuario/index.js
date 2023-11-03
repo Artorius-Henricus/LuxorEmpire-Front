63367e4b57b6c2945f2f540ba2c8ad5f4063dd72
Arthur Henrique <92705939+Artorius-Henricus@users.noreply.github.com>
2023-11-03 16:04:37 -0300

    .

--- site/src/pages/site/telausuario/index.js
+++ site/src/pages/site/telausuario/index.js
@@ -31,7 +31,7 @@ export default function PaginaTelaUsuario(){
             setUserName(storage('user-info').nome)
             setUserCpf(storage('user-info').cpf)
             setUserEmail(storage('user-info').email)
-            setUserTelefone(storage('user-info').telefone)
+            setUserTelefone(storage('user-info').telefonee)
             setUserNascimento(storage('user-info').nascimento)
 
             if(!storage('user-info').img) {
