f1ff7520e6793eea0e82e3ef463a845909de9feb
Arthur César <Arthurzika.2077@gmail.com>
2023-11-16 21:13:47 -0300

    mudanças em market e ainda fazendo informações

--- site/src/pages/site/informações/index.js
+++ site/src/pages/site/informações/index.js
@@ -7,17 +7,17 @@ export default function PaginaInformaçãoes () {
         <div className='Pagina-informações'>
             <CompCabecalho />
             <div className='titulo'>
-                <h1>Infomarções</h1>
+                <h1>Informações</h1>
             </div>
             <div className='content'>
-                 <div className='entregas'>
+                <div className='entregas'>
                     <ul>
                         <li>Formas de entrega</li>
                     </ul>
                     <p>Luxor Empire transporta seus produtos através de uma empresa de transportes terceirizada para garantir a segurança o produto </p>
                 </div>
         
-                 <div className='entregas'>
+                <div className='entregas'>
                     <ul>
                         <li>Políticas de segurança e privacidade</li>
                     </ul>
@@ -48,6 +48,77 @@ export default function PaginaInformaçãoes () {
                     <p>No Atendimento ao Consumidor</p>
                     <p>No desenvolvimento de produtos e serviços</p>
                 </div>
+
+                <div className='entregas'>
+                    <ul>
+                        <li>Termos de uso</li>
+                    </ul>
+                   <p>Termos de Uso do E-Commerce Luxor Empire</p>
+                   <p>Bem-vindo ao E-Commerce Luxor Empire! Antes de utilizar nossos serviços, por favor, leia atentamente os seguintes Termos de Uso. Ao acessar ou utilizar nosso site, você concorda em cumprir e estar vinculado a estes termos. Se você não concordar com algum aspecto destes termos, por favor, não utilize nossos serviços.</p>
+                   <div className='termos'>
+                        <p>1. **Cadastro e Conta do Usuário:**   </p>
+                        <p>1.1. Você é responsável por manter a confidencialidade de sua conta e senha.</p>
+                        <p>1.2. Informações fornecidas durante o cadastro devem ser precisas e atualizadas.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>2. **Condições Gerais:**</p>
+                        <p>2.1. Ao realizar uma compra, você concorda em fornecer informações precisas e verdadeiras.</p>
+                        <p>2.2. O E-Commerce Luxor Empire reserva-se o direito de recusar ou cancelar pedidos a seu critério exclusivo.</p>
+                   </div>
+          
+                   <div className='termos'>
+                        <p>3. **Privacidade:**</p>
+                        <p>3.1. Seus dados pessoais são tratados de acordo com nossa Política de Privacidade.</p>
+                        <p>3.2. Ao utilizar nossos serviços, você concorda com a coleta e uso de suas informações conforme descrito em nossa Política de Privacidade.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>4. **Pagamentos:**</p>
+                        <p>4.1. O E-Commerce Luxor Empire utiliza métodos de pagamento seguros.</p>
+                        <p>4.2. Ao realizar uma compra, você concorda com as condições de pagamento e preços apresentados.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>5. **Envio e Entrega:**</p>
+                        <p>5.1. O prazo de entrega pode variar conforme sua localização e outras circunstâncias.</p>
+                        <p>5.2. O E-Commerce Luxor Empire não é responsável por atrasos causados por eventos fora de nosso controle.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>6. **Devoluções e Trocas:**</p>
+                        <p> 6.1. Consulte nossa Política de Devolução para obter informações sobre devoluções e trocas ao final da página.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>7. **Propriedade Intelectual:**</p>
+                        <p> 7.1. Todo o conteúdo do E-Commerce Luxor Empire é protegido por direitos autorais e outras leis de propriedade intelectual.</p>
+                        <p>4.2. Ao realizar uma compra, você concorda com as condições de pagamento e preços apresentados.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>8. **Comunicações:**</p>
+                        <p>8.1. Ao se cadastrar, você concorda em receber comunicações do E-Commerce Luxor Empire, incluindo newsletters e informações sobre promoções.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>9. **Responsabilidade:**</p>
+                        <p>9.1. O E-Commerce Luxor Empire não se responsabiliza por danos diretos, indiretos, acidentais ou consequentes resultantes do uso de nossos serviços.</p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>10. **Alterações nos Termos de Uso:**</p>
+                        <p> 10.1. Reservamo-nos o direito de alterar estes Termos de Uso a qualquer momento, notificando os usuários sobre as mudanças. </p>
+                   </div>
+
+                   <div className='termos'>
+                        <p>11. **Encerramento de Conta:**</p>
+                        <p>11.1. Reservamo-nos o direito de encerrar contas de usuários que violem estes Termos de Uso ou estejam envolvidos em atividades fraudulentas.</p>
+                   </div>
+
+                   <p>Ao utilizar o E-Commerce Luxor Empire, você concorda com estes Termos de Uso. Se tiver alguma dúvida, entre em contato conosco. Obrigado por escolher o E-Commerce Luxor Empire!</p>
+                </div>
+        
             </div>         
         </div>
     )
